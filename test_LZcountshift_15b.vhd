e1ac9dcc877b6b6a8e81e576be46efad66a5da0d
David Mallasen <dmallasen@gmail.com>
2021-10-07 17:52:33 +0200

    Change bitwidth of LZCS

--- test_LZcountshift_15b.vhd
+++ test_LZcountshift_15b.vhd
@@ -3,29 +3,29 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.all;
 
-entity test_LZcountshift_14b is
-end test_LZcountshift_14b;
+entity test_LZcountshift_15b is
+end test_LZcountshift_15b;
 
-architecture test of test_LZcountshift_14b is
+architecture test of test_LZcountshift_15b is
 
-    component LZcountshift_14b
+    component LZcountshift_15b
         port (
             -- Input vector
-            x       : in  std_logic_vector(13 downto 0);
+            x       : in  std_logic_vector(14 downto 0);
             -- Number of leading zeros
             nlzeros : out unsigned(3 downto 0);
             -- Output vector left-shifted count bits
-            y       : out std_logic_vector(13 downto 0)
+            y       : out std_logic_vector(14 downto 0)
         );
     end component;
 
-    signal x : std_logic_vector(13 downto 0);
+    signal x : std_logic_vector(14 downto 0);
     signal nlzeros : unsigned(3 downto 0);
-    signal y : std_logic_vector(13 downto 0);
+    signal y : std_logic_vector(14 downto 0);
 
 begin
 
-    DUT: LZcountshift_14b
+    DUT: LZcountshift_15b
         port map(
             x => x,
             nlzeros => nlzeros,
@@ -35,7 +35,7 @@ begin
         process
         begin
 
-            x <= "1" & (12 downto 0 => '0');
+            x <= "1" & (13 downto 0 => '0');
 
             wait for 1 ns;
 
@@ -49,44 +49,44 @@ begin
 
             ----------
 
-            x <= (12 downto 0 => '0') & "1";
+            x <= (13 downto 0 => '0') & "1";
 
             wait for 1 ns;
 
-            assert(nlzeros = 13)
-            report "zeros & 1 and nlzeros != 13" 
+            assert(nlzeros = 14)
+            report "zeros & 1 and nlzeros != 14" 
             severity error;
 
-            assert(y = ("1" & (12 downto 0 => '0')))
+            assert(y = ("1" & (13 downto 0 => '0')))
             report "zeros & 1 and y != 1 & zeros" 
             severity error;
 
             ----------
 
-            x <= "00000111001101";
+            x <= "000001110011010";
 
             wait for 1 ns;
 
             assert(nlzeros = 5)
-            report "00000111001101 and nlzeros != 5" 
+            report "000001110011010 and nlzeros != 5" 
             severity error;
 
-            assert(y = "11100110100000")
-            report "00000111001101 and wrong y" 
+            assert(y = "111001101000000")
+            report "000001110011010 and wrong y" 
             severity error;
 
             ----------
 
-            x <= "01010011011001";
+            x <= "010100110110010";
 
             wait for 1 ns;
 
             assert(nlzeros = 1)
-            report "01010011011001 and nlzeros != 1" 
+            report "010100110110010 and nlzeros != 1" 
             severity error;
 
-            assert(y = "10100110110010")
-            report "01010011011001 and wrong y" 
+            assert(y = "101001101100100")
+            report "010100110110010 and wrong y" 
             severity error;
 
         end process;
