821d095abc393b2b0db6aa80a5d5d963bf5cf3b8
heavy94 <tomashenriques.kp@gmail.com>
2021-10-09 15:52:50 +0200

    encoder with testbench

---
+++ test_posit16_encoder.vhd
@@ -0,0 +1,134 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.all;
+
+entity test_posit16_encoder is
+    generic (
+        N   : integer := 16;
+        ES  : integer := 2;
+        R_N : integer := 4;
+        F_N : integer := 16-3-2+1 -- {N - (sign and minimum regime) - ES + (hidden fraction bit)}
+    );
+end test_posit16_encoder;
+
+architecture test of test_posit16_encoder is
+
+    signal sign    : std_logic;
+    signal sf      : std_logic_vector(ES + R_N downto 0);
+    signal frac    : std_logic_vector(F_N-1 downto 0);
+    signal inf     : std_logic;
+    signal x       : std_logic_vector(N-1 downto 0);
+
+    component posit16_encoder
+        port (
+            sign    : in  std_logic;
+            sf      : in std_logic_vector(ES + R_N downto 0);
+            frac    : in std_logic_vector(F_N-1 downto 0);
+            inf     : in std_logic;
+            x       : out std_logic_vector(N-1 downto 0)
+        );
+    end component;
+
+begin
+
+    DUT: posit16_encoder
+        port map(
+            sign => sign,
+            sf => sf,
+            frac => frac,
+            inf => inf,
+            x => x
+        );
+
+    process
+    begin
+        -- Test 1
+
+        sign <= '1';
+        sf <= "01110" & "00";
+        frac <= "000000000000";
+        inf <= '1';
+        wait for 1 ns;
+        assert(x = "1000000000000000")
+        report "Test 1 should be 1000000000000000"
+        severity error;
+
+        -- Test 2
+
+        sign <= '0';
+        sf <= "01101" & "00";
+        frac <= "100000000000";
+        inf <= '0';
+        wait for 1 ns;
+        assert(x = "0000000000000001")
+        report "Test 2 should be 0000000000000001"
+        severity error;
+        
+        -- Test 3
+
+        sign <= '0';
+        sf <= "00100" & "11";
+        frac <= "100110100000"; 
+        inf <= '0';
+        wait for 1 ns;
+        assert(x = "0000001110011010")
+        report "Test 3 should be 0000001110011010"
+        severity error;
+
+        -- Test 4
+
+        sign <= '1';
+        sf <= "11011" & "00";
+        frac <= "111001100000";
+        inf <= '0';
+        wait for 1 ns;
+        assert(x = "1000001110011010")
+        report "Test 4 should be 1000001110011010"
+        severity error;
+
+        -- Test 5
+
+        sign <= '0';
+        sf <= "00000" & "01";
+        frac <= "100110110010"; 
+        inf <= '0';
+        wait for 1 ns;
+        assert(x = "0010100110110010")
+        report "Test 5 should be 0010100110110010"
+        severity error;
+
+        -- Test 6
+
+        sign <= '1';
+        sf <= "11111" & "10";
+        frac <= "111001001110"; 
+        inf <= '0';
+        wait for 1 ns;
+        assert(x = "1010100110110010")
+        report "Test 6 should be 1010100110110010"
+        severity error;
+
+        -- Test 7
+
+        sign <= '0';
+        sf <= "11000" & "11";
+        frac <= "100100000000"; 
+        inf <= '0';
+        wait for 1 ns;
+        assert(x = "0111111110110010")
+        report "Test 7 should be 0111111110110010"
+        severity error;
+
+        -- Test 8
+
+        sign <= '1';
+        sf <= "00111" & "00";
+        frac <= "111100000000";
+        inf <= '0';
+        wait for 1 ns;
+        assert(x = "1111111110110010")
+        report "Test 8 should be 1111111110110010"
+        severity error;
+    end process;
+end test ;
--- test_right_shifter_round_12b.vhd
+++ test_right_shifter_round_12b.vhd
@@ -3,35 +3,35 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.all;
 
-entity test_right_shifter_round_12b is
-end test_right_shifter_round_12b;
+entity test_right_shifter_round_17b is
+end test_right_shifter_round_17b;
 
-architecture test of test_right_shifter_round_12b is
+architecture test of test_right_shifter_round_17b is
 
-    component right_shifter_round_12b
+    component right_shifter_round_17b
         port (
         -- Input vector
-        x        : in  std_logic_vector(11 downto 0);
+        x        : in  std_logic_vector(16 downto 0);
         -- Number of bits to shift
-        count    : in  std_logic_vector(3 downto 0);
+        count    : in  std_logic_vector(4 downto 0);
         -- Bit to prepend when shifting
         prep_bit : in std_logic;
         -- Output vector right-shifted count bits
-        y        : out std_logic_vector(11 downto 0);
+        y        : out std_logic_vector(16 downto 0);
         -- Sticky bit (OR of the discarded right bits)
         s        : out std_logic
     );
     end component;
 
-    signal x        : std_logic_vector(11 downto 0);
-    signal count    : std_logic_vector(3 downto 0);
+    signal x        : std_logic_vector(16 downto 0);
+    signal count    : std_logic_vector(4 downto 0);
     signal prep_bit : std_logic;
-    signal y        : std_logic_vector(11 downto 0);
+    signal y        : std_logic_vector(16 downto 0);
     signal s        : std_logic;
 
 begin
 
-    DUT: right_shifter_round_12b
+    DUT: right_shifter_round_17b
         port map(
             x        => x,
             count    => count,
@@ -43,114 +43,114 @@ begin
         process
         begin
 
-            x <= "000001110011";
-            count <= "0000";
+            x <= "00000000001110011";
+            count <= "00000";
             prep_bit <= '0';
 
             wait for 1 ns;
 
-            assert(y = "000001110011")
-            report "000001110011, count 0 and prep_bit 0 wrong y" 
+            assert(y = "00000000001110011")
+            report "00000000001110011, count 0 and prep_bit 0 wrong y" 
             severity error;
 
             assert(s = '0')
-            report "000001110011, count 0 and prep_bit 0 wrong s" 
+            report "00000000001110011, count 0 and prep_bit 0 wrong s" 
             severity error;
 
             ----------
 
-            x <= "010001110011";
-            count <= "1111";
+            x <= "00000010001110011";
+            count <= "11111";
             prep_bit <= '1';
 
             wait for 1 ns;
 
-            assert(y = "111111111111")
-            report "010001110011, count 15 and prep_bit 1 wrong y" 
+            assert(y = "11111111111111111")
+            report "00000010001110011, count 31 and prep_bit 1 wrong y" 
             severity error;
 
             assert(s = '1')
-            report "010001110011, count 15 and prep_bit 1 wrong s" 
+            report "00000010001110011, count 31 and prep_bit 1 wrong s" 
             severity error;
 
             ----------
 
-            x <= "100001110011";
-            count <= "1100";
+            x <= "00000100001110011";
+            count <= "10001";
             prep_bit <= '0';
 
             wait for 1 ns;
 
-            assert(y = "000000000000")
-            report "100001110011, count 12 and prep_bit 0 wrong y"
+            assert(y = "00000000000000000")
+            report "00000100001110011, count 17 and prep_bit 0 wrong y"
             severity error;
 
             assert(s = '1')
-            report "100001110011, count 12 and prep_bit 0 wrong s" 
+            report "00000100001110011, count 17 and prep_bit 0 wrong s" 
             severity error;
 
             ----------
 
-            x <= "100001110011";
-            count <= "0111";
+            x <= "00000100001110011";
+            count <= "00111";
             prep_bit <= '1';
 
             wait for 1 ns;
 
-            assert(y = "111111110000")
-            report "100001110011, count 7 and prep_bit 1 wrong y"
+            assert(y = "11111110000010000")
+            report "00000100001110011, count 7 and prep_bit 1 wrong y"
             severity error;
 
             assert(s = '1')
-            report "100001110011, count 7 and prep_bit 1 wrong s"
+            report "00000100001110011, count 7 and prep_bit 1 wrong s"
             severity error;
 
             ----------
 
-            x <= "100001110011";
-            count <= "0001";
+            x <= "00000100001110011";
+            count <= "00001";
             prep_bit <= '0';
 
             wait for 1 ns;
 
-            assert(y = "010000111001")
-            report "100001110011, count 1 and prep_bit 0 wrong y"
+            assert(y = "00000010000111001")
+            report "00000100001110011, count 1 and prep_bit 0 wrong y"
             severity error;
 
             assert(s = '1')
-            report "100001110011, count 1 and prep_bit 0 wrong s"
+            report "00000100001110011, count 1 and prep_bit 0 wrong s"
             severity error;
 
             ----------
 
-            x <= "100001110010";
-            count <= "0001";
+            x <= "00000100001110010";
+            count <= "00001";
             prep_bit <= '0';
 
             wait for 1 ns;
 
-            assert(y = "010000111001")
-            report "100001110010, count 1 and prep_bit 0 wrong y"
+            assert(y = "00000010000111001")
+            report "00000100001110010, count 1 and prep_bit 0 wrong y"
             severity error;
 
             assert(s = '0')
-            report "100001110010, count 1 and prep_bit 0 wrong s"
+            report "00000100001110010, count 1 and prep_bit 0 wrong s"
             severity error;
 
             ----------
 
-            x <= "100001110011";
-            count <= "0010";
+            x <= "00000100001110011";
+            count <= "00010";
             prep_bit <= '1';
 
             wait for 1 ns;
 
-            assert(y = "111000011100")
-            report "100001110011, count 2 and prep_bit 1 wrong y"
+            assert(y = "11000001000011100")
+            report "00000100001110011, count 2 and prep_bit 1 wrong y"
             severity error;
 
             assert(s = '1')
-            report "100001110011, count 2 and prep_bit 1 wrong s"
+            report "00000100001110011, count 2 and prep_bit 1 wrong s"
             severity error;
 
         end process;
