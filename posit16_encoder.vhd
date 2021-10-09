13e6077cfd01bf82459754e44a2c45b95533d07a
heavy94 <tomashenriques.kp@gmail.com>
2021-10-09 17:49:37 +0200

    Continue posit encoder

--- posit16_encoder.vhd
+++ posit16_encoder.vhd
@@ -22,8 +22,8 @@ end posit16_encoder;
 architecture behaviour of posit16_encoder is
 
     signal exp      : std_logic_vector(ES-1 downto 0);
-    signal tmp      : std_logic_vector(N downto 0);
-    signal ans_tmp  : std_logic_vector(N downto 0);
+    signal tmp      : std_logic_vector(N-1 downto 0);
+    signal ans_tmp  : std_logic_vector(N-1 downto 0);
     signal offset   : std_logic_vector(R_N downto 0);
     signal l_bit    : std_logic;
     signal g_bit    : std_logic;
@@ -31,12 +31,12 @@ architecture behaviour of posit16_encoder is
     signal s_bit    : std_logic;
     signal round    : std_logic;
 
-    component right_shifter_round_17b 
+    component right_shifter_round_16b 
         port (
             -- Input vector
             x        : in  std_logic_vector(16 downto 0);
             -- Number of bits to shift
-            count    : in  std_logic_vector(4 downto 0);
+            count    : in  std_logic_vector(3 downto 0);
             -- Bit to prepend when shifting
             prep_bit : in std_logic;
             -- Output vector right-shifted count bits
@@ -53,19 +53,19 @@ begin
     process(sf, exp, frac)
     begin
         if sf(ES + R_N) = '1' then
-            tmp <= '1' & exp & frac & "00";
-            offset <= not sf(ES + R_N downto 2);
+            tmp <= '1' & exp & frac(F_N - 2 downto 0) & "00";
+            offset <= std_logic_vector(unsigned(not(sf(ES + R_N downto 2))) + 1);
         else
-            tmp <= '0' & exp & frac & "00";
-            offset <= sf(ES + R_N downto 2);
+            tmp <= '0' & exp & frac(F_N - 2 downto 0) & "00";
+            offset <= std_logic_vector(unsigned(sf(ES + R_N downto 2)) + 1);
         end if;
     end process;
     -- Right-shift 'tmp', 'offset' bits prepending ¬sf[MSB] to the left
     -- and OR the discarded bits from the right.
-    shifter_round: right_shifter_round_17b
+    shifter_round: right_shifter_round_16b
         port map(
             x => tmp,
-            count => offset,
+            count => offset(R_N - 1 downto 0),
             prep_bit => not sf(ES + R_N),
             y => ans_tmp,
             s => s_bit
@@ -81,9 +81,9 @@ begin
         if inf = '1' then
             x <= x"8000";
         elsif sign = '1' then
-            x <= '1' & std_logic_vector(unsigned(not(ans_tmp(N downto 2))) + round + 1);
+            x <= '1' & std_logic_vector(unsigned(not(ans_tmp(N-1 downto 2))) + round + 1);
         else
-            x <= '0' & std_logic_vector(unsigned(ans_tmp(N downto 2)) + round);
+            x <= '0' & std_logic_vector(unsigned(ans_tmp(N-1 downto 2)) + round);
         end if;          
     end process;
 end behaviour;
---
+++ right_shifter_round_16b.vhd
@@ -0,0 +1,62 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.all;
+
+-- Right shifter
+entity right_shifter_round_16b is
+    port (
+        -- Input vector
+        x        : in  std_logic_vector(15 downto 0);
+        -- Number of bits to shift
+        count    : in  std_logic_vector(3 downto 0);
+        -- Bit to prepend when shifting
+        prep_bit : in std_logic;
+        -- Output vector right-shifted count bits
+        y        : out std_logic_vector(15 downto 0);
+        -- Sticky bit (OR of the discarded right bits)
+        s        : out std_logic
+    );
+end right_shifter_round_16b;
+
+architecture behaviour of right_shifter_round_16b is
+
+    signal x_4 : std_logic_vector(15 downto 0);
+    signal x_3 : std_logic_vector(15 downto 0);
+    signal x_2 : std_logic_vector(15 downto 0);
+    signal x_1 : std_logic_vector(15 downto 0);
+    signal x_0 : std_logic_vector(15 downto 0);
+
+    signal s_3 : std_logic;
+    signal s_2 : std_logic;
+    signal s_1 : std_logic;
+    signal s_0 : std_logic;
+
+begin
+    
+    x_4 <= x;
+
+    x_3 <= x_4 when count(3) = '0' else
+           (7 downto 0 => prep_bit) & x_4(15 downto 8);
+    s_3 <= '0' when count(3) = '0' else
+           or x_4(7 downto 0);
+
+    x_2 <= x_3 when count(2) = '0' else
+           (3 downto 0 => prep_bit) & x_3(15 downto 4);
+    s_2 <= '0' when count(2) = '0' else
+           or x_3(3 downto 0);
+
+    x_1 <= x_2 when count(1) = '0' else
+           (1 downto 0 => prep_bit) & x_2(15 downto 2);
+    s_1 <= '0' when count(1) = '0' else
+           or x_2(1 downto 0);
+
+    x_0 <= x_1 when count(0) = '0' else
+           (0 downto 0 => prep_bit) & x_1(15 downto 1);
+    s_0 <= '0' when count(0) = '0' else
+           or x_1(0 downto 0);
+   
+    y <= x_0;
+    s <= s_3 or s_2 or s_1 or s_0;
+
+end behaviour;
